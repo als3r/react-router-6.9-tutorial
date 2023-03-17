af197895fa73412b20fa056555724676e2b20cbb
Alex <sergeyev.alex@gmail.com>
2023-03-17 16:39:42 -0400

    Initial commit

---
+++ src/routes/destroy.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { redirect } from 'react-router-dom'
+import { deleteContact } from '../contacts'
+
+export async function action({ params }) {
+  await deleteContact(params.contactId)
+  return redirect(`/`)
+}
